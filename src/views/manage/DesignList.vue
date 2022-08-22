9295db39b3b800b634abc1ca5a9a8bfecf741b0b
chao <vchenghou@aliyun.com>
2022-08-22 21:42:13 +0800

    还原路由模式适应gitee page

--- src/views/manage/DesignList.vue
+++ src/views/manage/DesignList.vue
@@ -67,10 +67,10 @@ export default {
   },
   methods:{
     toView(item){
-      window.open( '/view?id='+item.id);
+      window.open( '/#/view?id='+item.id);
     },
     toDesign(item){
-      window.open('/design?id='+item.id);
+      window.open('/#/design?id='+item.id);
     },
     deleteItem(item){
       this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
